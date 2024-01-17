fbe6b761d28aef2f8533268df3f64baa7b78b07f
Sai-Rane <sai.rane729@gmail.com>
2024-01-17 15:35:28 +0530

    bug fix

--- NodeJS/1CreateServer.js
+++ NodeJS/1CreateServer.js
@@ -1,3 +1,4 @@
+// https://www.youtube.com/watch?v=vJEO57B05Sg&t=1804s
 // createServer is a function which creates service for you
 // createServer is a function which is available in the http module
 // createServer takes a function and that function accepts 2 parameters. 1st parameter is something which you received from the Client and the 2nd is something which you want to send to the Client.
@@ -8,7 +9,8 @@ var http = require("http"); //mentioning that you require the http module
 
 http
   .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" }); //this is how we send header. We mention 2 things there..status code and content type
     res.write("Welcome back");
     res.end();
   })
-  .listen(8080); //mention port number
+  .listen(8080); //mentioning port number
