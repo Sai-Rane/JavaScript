b2c52abd41fb8c9a705e17a46c8a6603be6116a9
Sai-Rane <sai.rane729@gmail.com>
2024-01-25 17:29:01 +0530

    bug fix

--- Youtube/CallbackFunction/Callback.js
+++ Youtube/CallbackFunction/Callback.js
@@ -1,7 +1,8 @@
+//www.youtube.com/watch?v=z-H8S83jJec&list=PLhzIaPMgkbxDxVcH-M-JFM73PY1R_i2mK&index=43
 //different methods of writing function are shown below
 
 //method 1
-function name() {
+https: function name() {
   console.log("hello");
 }
 name();
@@ -12,18 +13,41 @@ const tim = function () {
 };
 tim();
 
+const square = (num) => num * num;
+console.log(square(2));
+
 //method 3 using arrow function
 const tam = () => {
   console.log("tam");
 };
 tam();
 
+// ---------------------------------------------------------------
+
 //Now let's see callback function
+
+// Passing one function as parameter to another function
 const calculate = (a, b, operation) => {
   return operation(a, b);
 };
 
-const result = calculate(2, 3, function (num1, num2) {
+//method 1
+const addition = calculate(2, 3, function (num1, num2) {
   return num1 + num2;
 });
-console.log("result", result);
+console.log("addition", addition);
+
+//method 2
+const subtraction = (a, b) => a - b;
+
+const subResult = calculate(10, 5, subtraction); //argument
+console.log("subResult", subResult);
+
+//method 3
+function multiply(a, b) {
+  //a and b are parameters
+  return a * b;
+}
+
+const mulResult = calculate(2, 3, multiply);
+console.log("mulResult", mulResult);
