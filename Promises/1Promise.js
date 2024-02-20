174fe06bcd624a50862485ff863a0015c7b885e5
Sai-Rane <sai.rane729@gmail.com>
2024-02-20 17:38:40 +0530

    bug fix

--- Promises/1Promise.js
+++ Promises/1Promise.js
@@ -1,11 +1,15 @@
-//Promise - It is like a object which will have some data
-//Promise returns PromiseState and PromiseResult
+// Promise - It is like a object which will have some data
+// Promise returns PromiseState and PromiseResult
 // PromiseResult will store whatever data Promise will return
 // PromiseState will tell us the state of promise whether it is Pending,Rejected or Fulfilled
 // Promise is an object which represents eventual completion of an async operation
-const apiURl = "https://api.github.com/users/Sai-Rane";
 
-const data = fetch(apiURl);
+// https://www.youtube.com/watch?v=ap-6PPAuK1Y
+// https://www.youtube.com/watch?v=6nv3qy3oNkc
+
+const apiURL = "https://api.github.com/users/Sai-Rane";
+
+const data = fetch(apiURL); //the fetch function returns a Promise
 console.log("data", data);
 
 data.then(function (data) {
--- asyncAwait/1Async.js
+++ asyncAwait/1Async.js
@@ -1,5 +1,9 @@
 // https://www.youtube.com/watch?v=6nv3qy3oNkc&t=1448s
 //To make a function async write async keyword before it. Async function will always return a Promise. The promise will either get rejected or fulfilled. Suppose the function is not returning Promise and it is returning a value then the value will be wrapped in a Promise. Async and await combo is used to handle Promises
+//async also known as nonblocking code, asynchronous programming provides opportunities for a program to continue running other code while waiting for a long-running task to complete. The time-consuming task is executed in the background while the rest of the code continues to execute.
+
+// https://www.youtube.com/watch?v=ap-6PPAuK1Y
+// https://www.youtube.com/watch?v=6nv3qy3oNkc
 
 async function getData() {
   return "Hello";
